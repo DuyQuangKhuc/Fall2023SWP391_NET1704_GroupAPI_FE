5e1060ea1c9270508d44654c41be286cdac0d8e8
Hoang14127 <ngusimeri@gmail.com>
2023-10-02 17:11:21 +0700

    update-accountmanage

--- src/Page/admin/AccountManage.jsx
+++ src/Page/admin/AccountManage.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import ComHeaderAdmin from '../Components/ComHeaderAdmin/ComHeaderAdmin';
 import { notification, Modal } from 'antd';
 import { textApp } from '../../TextContent/textApp'
-
+import * as yup from "yup"
+import { useFormik } from 'formik';
 export default function AccountManage() {
     const accounts = [
         { id: 1, name: 'Tài khoản 1' },
@@ -10,34 +11,88 @@ export default function AccountManage() {
         { id: 3, name: 'Tài khoản 3' },
     ];
 
+    const AccountSchema = yup.object().shape({
+        updatedName: yup.string().required('Vui lòng nhập tên tài khoản'),
+        updatedPhone: yup.string().required('Vui lòng nhập số điện thoại'),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            updatedName: '',
+            updatedPhone: '',
+        },
+        validationSchema: AccountSchema,
+        onSubmit: (values) => {
+            console.log('Dữ liệu cập nhật:', values);
+            handleConfirmUpdate();
+        },
+    });
+
+    const [selectedAccount, setSelectedAccount] = useState(null);
     const [api, contextHolder] = notification.useNotification();
+
     const [deleteAccount, setDeleteAccount] = useState(null);
-    const [isOpen, setisOpen] = useState(false);
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
-    const handleDelete = (accountId) => {
-        setDeleteAccount(accountId);
-        setisOpen(true);
-    };
+    const [updateAccount, setUpdateAccount] = useState(null);
+    const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
 
-    const handleCancel = () => {
-        setisOpen(false);
-    };
+    const [updatedName, setUpdatedName] = useState('');
+    const [updatedPhone, setUpdatedPhone] = useState('');
 
-    const handleConfirmDelete = () => {
-        // Xóa tài khoản với ID 
-        setisOpen(false);
-        setDeleteAccount(null);
-        // Thông báo xóa thành công
+    //update
+    const handleConfirmUpdate = () => {
+        if (updateAccount) {
+            console.log('Cập nhật tài khoản:', updateAccount.id);
+            console.log('Tên mới:', updatedName);
+            console.log('Số điện thoại mới:', updatedPhone);
+        }
+
+        setIsUpdateModalOpen(false);
+        setUpdateAccount(null);
+        setUpdatedName('');
+        setUpdatedPhone('');
         api.success({
-            message: 'Xóa thành công',
-            description: 'Tài khoản đã được xóa thành công.',
-            duration: 3,
+            message: 'Cập nhật thành công',
+            description: 'Tài khoản đã được cập nhật thành công.',
+            duration: 2,
         });
     };
 
     const handleUpdate = (accountId) => {
-        // cập nhật 
+        const account = accounts.find((account) => account.id === accountId);
+        setSelectedAccount(account);
+        console.log(selectedAccount)
+        setUpdateAccount(accountId);
+        setIsUpdateModalOpen(true);
     };
+
+    //delete
+    const handleDelete = (accountId) => {
+        setDeleteAccount(accountId);
+        setIsDeleteModalOpen(true);
+    };
+
+
+    const handleConfirmDelete = () => {
+        setIsDeleteModalOpen(false);
+        setDeleteAccount(null);
+        api.success({
+            message: 'Xóa thành công',
+            description: 'Tài khoản đã được xóa thành công.',
+            duration: 2,
+        });
+    };
+
+    //close popup
+    const handleDeleteClose = () => {
+        setIsDeleteModalOpen(false)
+    }
+
+    const handleUpdateClose = () => {
+        setIsUpdateModalOpen(false)
+    }
+
 
     const handleCreate = () => {
         // tạo tài khoản mới
@@ -96,9 +151,14 @@ export default function AccountManage() {
                     Tạo tài khoản mới
                 </button>
 
+
+
+
+                {/* Thông báo delete */}
                 <Modal
                     title="Xác nhận xóa"
-                    open={isOpen}
+                    open={isDeleteModalOpen}
+                    onCancel={handleDeleteClose}
                 >
                     <p>Bạn có chắc chắn muốn xóa tài khoản này?</p>
 
@@ -111,12 +171,77 @@ export default function AccountManage() {
                         </button>
                         <button
                             className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={handleCancel}
+                            onClick={handleDeleteClose}
                         >
                             Hủy bỏ
                         </button>
                     </div>
                 </Modal>
+
+                {/* Thông báo cập nhật  */}
+
+                <Modal
+                    title="Cập nhật tài khoản"
+                    open={isUpdateModalOpen}
+                    onCancel={handleUpdateClose}
+                >
+
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className="mb-4">
+                            <label htmlFor="updatedName" className="block text-gray-700 text-sm font-bold mb-2">
+                                Tên tài khoản
+                            </label>
+                            <input
+                                id="updatedName"
+                                name="updatedName"
+                                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${formik.touched.updatedName && formik.errors.updatedName ? 'border-red-500' : ''
+                                    }`}
+                                type="text"
+                                value={formik.values.updatedName}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.updatedName && formik.errors.updatedName && (
+                                <p className="text-red-500 text-xs italic">{formik.errors.updatedName}</p>
+                            )}
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="updatedPhone" className="block text-gray-700 text-sm font-bold mb-2">
+                                Số điện thoại
+                            </label>
+                            <input
+                                id="updatedPhone"
+                                name="updatedPhone"
+                                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${formik.touched.updatedPhone && formik.errors.updatedPhone ? 'border-red-500' : ''
+                                    }`}
+                                type="text"
+                                value={formik.values.updatedPhone}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.updatedPhone && formik.errors.updatedPhone && (
+                                <p className="text-red-500 text-xs italic">{formik.errors.updatedPhone}</p>
+                            )}
+                        </div>
+
+                        <div className="flex justify-end mt-4">
+                            <button
+                                type="submit"
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded"
+                            >
+                                Xác nhận
+                            </button>
+                            <button
+                                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                                onClick={handleUpdateClose}
+                            >
+                                Hủy bỏ
+                            </button>
+                        </div>
+                    </form>
+                </Modal>
+
             </div>
         </div>
     );
